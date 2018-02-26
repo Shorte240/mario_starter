656eb3b494e7300aa833c287c8bf4a8168e44b70
Shorte240 <fraserbarker@hotmail.com>
2018-02-26 12:15:38 +0000

    Added goalCube and menu image

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Detect if player enters Goal
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Application.LoadLevel("menu");
+        }
+    }
+}
