a21336c7835f2fea2887435401acf8adcb6a22ba
Shorte240 <fraserbarker@hotmail.com>
2018-02-26 15:01:56 +0000

    Added a basic timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime;
+
+	// Use this for initialization
+	void Start () {
+        startTime = 30;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float t = startTime - Time.time;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+
+        timerText.text = minutes + ":" + seconds;
+	}
+}
